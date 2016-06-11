988c6592c0228dc57ef7dcd96ec6a6fc13954730
joaoferreira <f16123@gmail.com>
2016-06-11 16:09:23 +0200

    1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,37 @@
+var $ = require('jquery');
+var row = 6;
+var column = 7;
 
-function initBoardArray()
-{
-	var iMax = 5;
-	var jMax = 7;
+function initBoardArray() {
 	var boardArray= new Array();
 
-	for (i=0;i<iMax;i++) {
-	 boardArray[i]=new Array();
-	 for (j=0;j<jMax;j++) {
-	  boardArray[i][j]=0;
-	 }
+	for (var i = 0; i<row; i++) {
+		boardArray[i]=new Array();
+		for (var j = 0; j < column; j++) {
+		 	boardArray[i][j] = 0;
+		}
 	}
-
-	alert(boardArray[0][0]);
 	return boardArray;
-}
+};
 
+// function printBoard(boardArray) {
+// 	for (i=0;i<row;i++) {
+// 		 for (j=0;j<column;j++) {
+// 		    var cellId = '#c' + i + j;
+// 		    debugger
+// 		    $(cellId).addClass('blue');
+// 		 }
+// 	}
+// };
+// printBoard(boardArray);
 
+$( document ).ready(function() {
 
+	$('.cell').addEventListener = function(){
+		alert('ola');
+	};
 
-function printBoard(boardArray)
-{
-	for (i=0;i<iMax;i++) {
-		 for (j=0;j<jMax;j++) {
-		    var x = document.getElementById("c"+i+j).cells;
-		    x.bgColor = "Yellow";   
-		 }
-	}
-
-}
+});
 
 var boardArray = initBoardArray();
 
-printBoard(boardArray);
